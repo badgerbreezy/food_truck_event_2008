88a03996673f2ae682175f8617d3968a1c9a8792
Brian Liu <brian.b.liu@gmail.com>
2020-09-15 13:18:49 -0600

    Add test FoodTruck can check stock.

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -12,4 +12,12 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", food_truck.name
     assert_equal ({}), food_truck.inventory
   end
+
+  def test_it_can_check_stock
+    item1 = Item.new({ name: 'Peach Pie (Slice)', price: '$3.75' })
+    item2 = Item.new({ name: 'Apple Pie (Slice)', price: '$2.50' })
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_equal 0, food_truck.check_stock(item1)
+  end
 end
