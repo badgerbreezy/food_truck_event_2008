9ccfa1419e0eecb29547b7ccdcef80d582dc123e
Brian Liu <brian.b.liu@gmail.com>
2020-09-15 13:41:38 -0600

    Add method food_truck_names to event.

--- lib/event.rb
+++ lib/event.rb
@@ -9,4 +9,11 @@ class Event
     @food_trucks << food_truck
   end
 
+  def food_truck_names
+    food_truck_names = @food_trucks.map do |food_truck|
+      food_truck.name
+    end
+    food_truck_names
+  end
+
 end
